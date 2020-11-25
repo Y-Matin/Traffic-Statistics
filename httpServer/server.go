45a4fadefcf7f3f84735723e3cb501f9470751a2
Y-Martin <724797522@qq.com>
2020-11-25 22:55:06 +0800

    finish all ！ 撒花 ！！！

--- httpServer/server.go
+++ httpServer/server.go
@@ -2,12 +2,17 @@ package main
 
 import (
 	"github.com/mediocregopher/radix.v2/redis"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"strconv"
+	"strings"
+	"time"
 )
 
 func main() {
 	http.HandleFunc("/path", dataStatistics)
+	http.ListenAndServe("127.0.0.1:8888", nil)
 }
 
 func dataStatistics(writer http.ResponseWriter, request *http.Request) {
@@ -16,6 +21,22 @@ func dataStatistics(writer http.ResponseWriter, request *http.Request) {
 		log.Fatal(err)
 	}
 	defer client.Close()
+
 	// 1. 用户数量 2.流量总数 3. 当天统计
-	//client.Cmd()
+	now := time.Now()
+	strDay := now.Format("20060102")
+	uvCount, err := client.Cmd("zcard", "uv_day_"+strDay).Int()
+	if err != nil {
+		log.Fatal(err)
+	}
+	pvCount, err := client.Cmd("zcard", "pv_day_"+strDay).Int()
+	if err != nil {
+		log.Fatal(err)
+	}
+	bytes, err := ioutil.ReadFile("./showData.html")
+	s := string(bytes)
+	s = strings.Replace(s, "${pv}", strconv.Itoa(pvCount), -1)
+	s = strings.Replace(s, "${uv}", strconv.Itoa(uvCount), -1)
+
+	writer.Write([]byte(s))
 }
